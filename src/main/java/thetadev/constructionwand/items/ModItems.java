e4b3421324d853afd42e115678d910e528de559f
Theta-Dev <t.testboy@gmail.com>
2020-12-06 17:42:39 +0100

    Added TE White/Blacklist Now triggers onBlockPlacedBy() Fixed SecurityCraft bug

--- src/main/java/thetadev/constructionwand/items/ModItems.java
+++ src/main/java/thetadev/constructionwand/items/ModItems.java
@@ -7,8 +7,6 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.IForgeRegistryEntry;
-import thetadev.constructionwand.basics.ConfigServer;
 import thetadev.constructionwand.ConstructionWand;
 
 @Mod.EventBusSubscriber(modid = ConstructionWand.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
